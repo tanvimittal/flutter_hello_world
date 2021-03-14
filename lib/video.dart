3ff74afc4229e579c221308eb92318724bfbdcb2
kyuuki <kyuuki.japan@gmail.com>
2021-03-15 08:54:54 +0900

    Add video data

--- lib/video.dart
+++ lib/video.dart
@@ -9,24 +9,76 @@ class Video {
   static List<Video> getVideoList() {
     List<Video> videos = [];
 
-    videos.add(Video("ONE PIECE", "https://www.netflix.com/watch/80998772", 2000, "images/OnePiece.jpg"));
-    videos.add(Video("ONE PIECE ねじまき島の冒険", "https://www.netflix.com/watch/70086565", 2001, "images/OnePiece.jpg"));
-    videos.add(Video("ONE PIECE 珍獣島のチョッパー王国", "https://www.netflix.com/watch/70086565", 2002, "images/OnePiece.jpg"));
-    videos.add(Video("ONE PIECE", "https://www.netflix.com/watch/80047616", 2000, "images/OnePiece.jpg"));
-    videos.add(Video("ONE PIECE", "https://www.netflix.com/watch/80047616", 2000, "images/OnePiece.jpg"));
-    videos.add(Video("ONE PIECE", "https://www.netflix.com/watch/80047616", 2000, "images/OnePiece.jpg"));
-    videos.add(Video("ONE PIECE", "https://www.netflix.com/watch/80047616", 2000, "images/OnePiece.jpg"));
-    videos.add(Video("ONE PIECE", "https://www.netflix.com/watch/80047616", 2000, "images/OnePiece.jpg"));
-    videos.add(Video("ONE PIECE", "https://www.netflix.com/watch/80047616", 2000, "images/OnePiece.jpg"));
-    videos.add(Video("ONE PIECE", "https://www.netflix.com/watch/80047616", 2000, "images/OnePiece.jpg"));
-    videos.add(Video("ONE PIECE", "https://www.netflix.com/watch/80047616", 2000, "images/OnePiece.jpg"));
-    videos.add(Video("ONE PIECE", "https://www.netflix.com/watch/80047616", 2000, "images/OnePiece.jpg"));
-    videos.add(Video("ONE PIECE", "https://www.netflix.com/watch/80047616", 2000, "images/OnePiece.jpg"));
-    videos.add(Video("ONE PIECE", "https://www.netflix.com/watch/80047616", 2000, "images/OnePiece.jpg"));
-    videos.add(Video("ONE PIECE", "https://www.netflix.com/watch/80047616", 2000, "images/OnePiece.jpg"));
-    videos.add(Video("ONE PIECE FILM Z", "https://www.netflix.com/watch/80198443", 2012, "images/OnePiece.jpg"));
-    videos.add(Video("ONE PIECE FILM GOLD", "https://www.netflix.com/watch/80163050", 2016, "images/OnePiece.jpg"));
-    videos.add(Video("ONE PIECE STAMPEDE", "https://www.netflix.com/watch/80155769", 2019, "images/OnePiece.jpg"));
+    videos.add(Video(
+        "ONE PIECE",
+        "https://www.netflix.com/watch/80998772",
+        2000,
+        "images/2000.jpg"));
+    videos.add(Video(
+        "ONE PIECE ねじまき島の冒険",
+        "https://www.netflix.com/watch/80998773",
+        2001,
+        "images/2001.jpg"));
+    videos.add(Video(
+        "ONE PIECE 珍獣島のチョッパー王国",
+        "https://www.netflix.com/watch/80998774",
+        2002,
+        "images/2002.jpg"));
+    videos.add(Video(
+        "ONE PIECE THE MOVIE デッドエンドの冒険",
+        "https://www.netflix.com/watch/80998775",
+        2003,
+        "images/2003.jpg"));
+    videos.add(Video(
+        "ONE PIECE 呪われた聖剣",
+        "https://www.netflix.com/watch/80998776",
+        2004,
+        "images/2004.jpg"));
+    videos.add(Video(
+        "ONE PIECE THE MOVIE オマツリ男爵と秘密の島",
+        "https://www.netflix.com/watch/80998778",
+        2005,
+        "images/2005.jpg"));
+    videos.add(Video(
+        "ONE PIECE THE MOVIE カラクリ城のメカ巨兵",
+        "https://www.netflix.com/watch/80998779",
+        2006,
+        "images/2006.jpg"));
+    videos.add(Video(
+        "ONE PIECE エピソード オブ アラバスタ 砂漠の王女と海賊たち",
+        "https://www.netflix.com/watch/70086565",
+        2007,
+        "images/2007.jpg"));
+    videos.add(Video(
+        "ONE PIECE THE MOVIE エピソード オブ チョッパー プラス 冬に咲く、奇跡の桜",
+        "https://www.netflix.com/watch/80998780",
+        2008,
+        "images/2008.jpg"));
+    videos.add(Video(
+        "ONE PIECE FILM STRONG WORLD／ワンピース フィルム ストロングワールド",
+        "https://www.netflix.com/watch/80998781",
+        2009,
+        "images/2009.jpg"));
+    videos.add(Video(
+        "ONE PIECE 3D 麦わらチェイス",
+        "https://www.netflix.com/watch/80198444",
+        2011,
+        "images/2011.jpg"));
+    videos.add(Video(
+        "ONE PIECE FILM Z",
+        "https://www.netflix.com/watch/80198443",
+        2012,
+        "images/2012.jpg"));
+    videos.add(Video(
+        "ONE PIECE FILM GOLD",
+        "https://www.netflix.com/watch/80163050",
+        2016,
+        "images/2016.jpg"));
+    videos.add(Video(
+        "ONE PIECE STAMPEDE",
+        "https://www.netflix.com/watch/80155769",
+        2019,
+        "images/2019.jpg"));
 
     return videos;
   }
